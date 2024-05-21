52a224a11c65c6a6cb1726fc702bbde2aa378206
ORTIZ BONILLA CARLOS JOSE <carortiz@unisalle.edu.co>
2024-05-21 11:17:48 -0500

    changing some aspects

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
 import streamlit as st
 import numpy as np
+import pickle
+import sklearn
+
+def tokenizer(text):
+  return text.split()
+
+st.title("Predict review sentiment")
+
+review = st.text_input("Ingrese la reseña: ")
+review_array = np.array([review])
+
+with open("model.pickle", "rb") as f:
+    model = pickle.load(f)
+
+if st.button("Predicción"):
+    pred = model.predict(review_array)[0]
+
+    if pred == 0:
+        st.write("Negativa")
+    else:
+        st.write("Positiva")
+
+
